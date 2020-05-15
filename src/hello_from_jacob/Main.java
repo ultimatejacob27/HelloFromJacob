2d462072705f17c90c7b194e4740e640e4aa3177
Emergency-Paperclip <ejnelson1997@gmail.com>
2020-05-15 15:33:17 -0600

    It worked

--- src/hello_from_jacob/Main.java
+++ src/hello_from_jacob/Main.java
@@ -2,7 +2,9 @@ package hello_from_jacob;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello, my name is Jacob");
+        System.out.println("Hello Jacob, my name is Ethan");
     }
 }
